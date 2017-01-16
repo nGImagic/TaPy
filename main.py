fa06497a2105a7e191977a783f7af661f5f3a1b2
RalphHarti <harti@mpc1990.local>
2017-01-16 09:41:50 +0100

    misc

--- main.py
+++ main.py
@@ -12,5 +12,11 @@ path_ob = '/Users/harti/switchdrive/repos/nGI_magic/r/data/data_OB'
 path_im = '/Users/harti/switchdrive/repos/nGI_magic/r/data/data_smp'
 path_dc = '/Users/harti/switchdrive/repos/nGI_magic/r/data/DCs'
 
+norm_param = [3,5,2,2]
+crop_param = [10,15,50,50]
+
+
 (im,ob) = read_data(path_im,path_ob,path_dc)
-im,ob = cropped(im,ob)
+im,ob = normalization(im,ob,*norm_param)
+im,ob = cropped(im,ob,*crop_param)
+
