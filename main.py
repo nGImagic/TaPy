a148e46bf2ff1898f1ef3dddf5b19eb548b83674
RalphHarti <harti@mpc1990.local>
2017-01-16 09:43:50 +0100

    Added relative instead of absolute paths

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ Created on Fri Jan  6 15:42:58 2017
 
 from functions import read_data,cropped,normalization
 
-path_ob = '/Users/harti/switchdrive/repos/nGI_magic/r/data/data_OB'
-path_im = '/Users/harti/switchdrive/repos/nGI_magic/r/data/data_smp'
-path_dc = '/Users/harti/switchdrive/repos/nGI_magic/r/data/DCs'
+path_ob = 'data/data_OB'
+path_im = 'data/data_smp'
+path_dc = 'data/DCs'
 
 (im,ob) = read_data(path_im,path_ob,path_dc)
 im,ob = cropped(im,ob)
