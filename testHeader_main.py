515398e34b31a886ab3fa57908811a3040ef7ef9
valsecchi <valsecchi.jacopo@gmail.com>
2017-01-20 07:27:49 +0100

    j

--- testHeader_main.py
+++ testHeader_main.py
@@ -46,9 +46,8 @@ def read_data(path_im,path_ob,path_dc):
     """
     read()
     """
-    return
 
-    return(stack_im_ar,stack_ob_ar)
+    return
     
 path_ob = 'data/data_OB'
 path_im = 'data/data_smp'
