879757aca2cb3ccf2db79fea85a2fb02aaf51ac1
valsecchi <valsecchi.jacopo@gmail.com>
2017-01-24 13:26:27 +0100

    inter

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ path_im = 'data/phase/data_smp'
 path_dc = ''#'data/DCs'
 
 norm_param = [3,5,20,40]
-crop_param = [10,15,500,500]
+crop_param = [10,15,80,60]
 bin_fac = None                 # no binning either 1 or None, 2x2 binning: bin_fac = 2
 
 (im,ob) = read_data(path_im,path_ob,path_dc)
 #im,ob=normalization(im,ob,*norm_param)
-#im,ob = cropped(im,ob,*crop_param)
+im,ob = cropped(im,ob,*crop_param)
 #im, ob = binning(im,ob,bin_fac)
 ti, dpci, dfi, vis_map = createIm(im,ob)
 #ti, dpci, dfi, vis_map = createIm_fft(im,ob)
 saveIm(ti, dpci, dfi, vis_map,name='name',folder='TIO',overWrite=True)
 pixel = pixelWiseDPC(dpci)
-saveIm(ti, pixel, dfi, vis_map,name='name',folder='TIO',overWrite=True)
+#saveIm(ti, pixel, dfi, vis_map,name='name',folder='TIO',overWrite=True)
 
 
 
