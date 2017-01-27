ee5993755ecf952df350646b721a61cffb171047
Ralph P. Harti <tpnspign@gmail.com>
2017-01-27 15:07:13 +0100

    Fixed

--- main.py
+++ main.py
@@ -8,15 +8,12 @@ Created on Fri Jan  6 15:42:58 2017
 from functions import read_data,cropped,createIm,normalization,saveIm,binning,oscillation,createIm_fft
 from pixelwiseDPC import pixelWiseDPC,pixelWisePC
 import numpy as np
-#path_ob = '/Users/valsecchi/Dropbox/B0A/OBspin_OFF'
-#path_im = '/Users/valsecchi/Dropbox/B0A/Sample_spin_OFF'
-#path_dc = '/Users/valsecchi/Documents/PSI/BOA_FOLDER/BOA_DICEMBRE/Data/01_firstSlot/LASER_IN/04_brp_dark'#'data/DCs'
 
-#path_ob = '/Users/valsecchi/nGI_magic/data/data_OB'
-#path_im = '/Users/valsecchi/nGI_magic/data/data_smp'
 
-path_ob = '/Users/valsecchi/nGI_magic/data/phase/data_OB'
-path_im = '/Users/valsecchi/nGI_magic/data/phase/data_smp'
+path_ob = 'data/data_OB'
+path_im = 'data/data_smp'
+path_dc = ''#'data/DCs'
+
 
 path_dc = ''#'data/DCs'
 bin_fac = None                 # no binning either 1 or None, 2x2 binning: bin_fac = 2
@@ -31,6 +28,10 @@ oscillation(im,ob,*oscillationParam,repeatedPeriod=True)
 #im,ob = cropped(im,ob,*crop_param)
 #im,ob=normalization(im,ob,*norm_param)
 #im, ob = binning(im,ob,bin_fac)
+#im, ob = win_filt_z(im,ob)
 ti, dpci, dfi, vis_map = createIm(im,ob)
 #ti, dpci, dfi, vis_map = createIm_fft(im,ob)
+oscillation(im,ob,5,5,2,2)
 saveIm(ti, dpci, dfi, vis_map,name='name',folder='folder',overWrite=True)
+
+
