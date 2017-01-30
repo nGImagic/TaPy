dbe7a55e5f5783d71d80b398e26e8201718d7ba3
valsecchi <valsecchi.jacopo@gmail.com>
2017-01-30 11:13:24 +0100

    split

--- functions.py
+++ functions.py
@@ -392,3 +392,35 @@ def win_filt_z(stack_im,stack_ob):
     im = np.reshape(stack_imReshaped, [shapeStack_im_org[0], shapeStack_im_org[1],shapeStack_im_org[2]])
     
     return im, ob
+
+def splitNewRoutine_BOA(path,firstSpinFlipperON=True):
+    """
+    when you acquire nGI data at BOA with the new routing (spinON spinOFF and step) this function splits the data into two folders
+    """
+    import re
+    
+    imExt = ['.fits','.tiff','.tif','.hdf','.h4','.hdf4','.he2','h5','.hdf5','.he5']
+    filenames = [name for name in os.listdir(path) if name.lower().endswith(tuple(imExt)) or re.search(r'\.bad\d+$', name)]
+    if all(re.search(r'\.bad\d+$', name) for name in filenames):
+        reshapedFilenamesBad =[]
+        print('All files ends .bad####')
+        for name in filenames:
+            nameSplit = re.split('_00001|\.bad',name)
+            nameSplit = nameSplit[0]+nameSplit[2].zfill(5)+nameSplit[1]
+            reshapedFilenamesBad.append(nameSplit)
+
+    for bad,badResh in zip(filenames,reshapedFilenamesBad):
+        print('old:', bad, '\nnew',badResh,2*'\n')
+        os.rename(path+'/'+bad,path+'/'+badResh)
+    filenames = reshapedFilenamesBad
+    
+    spin_ON,spin_OFF = '/spin_ON','/spin_OFF'
+    if not os.path.exists(path+spin_ON):
+        os.makedirs(path+spin_ON) 
+    if not os.path.exists(path+spin_OFF):
+        os.makedirs(path+spin_OFF)        
+    if firstSpinFlipperON:
+        for odd in filenames[::2]:
+            os.rename(path+'/'+odd,path+'/'+spin_ON+'/'+odd)
+        for even in filenames[1::2]:
+            os.rename(path+'/'+even,path+'/'+spin_OFF+'/'+even)
