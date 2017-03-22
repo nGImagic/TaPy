ba6821cadcd85a20ee53e16d1c771df94e68a2b2
Ralph P. Harti <tpnspign@gmail.com>
2017-03-22 13:52:58 +0100

    Update main.py
    Removed unnecessary lines

--- main.py
+++ main.py
@@ -10,12 +10,8 @@ from pixelwiseDPC import pixelWiseDPC,pixelWisePC
 import numpy as np
 
 
-#path_ob = 'data/data_OB'
-#path_im = 'data/data_smp'
-#path_dc = ''#'data/DCs'
-
-path_ob = 'data/data_OB_period2'
-path_im = 'data/data_smp_period2'
+path_ob = 'data/data_OB'
+path_im = 'data/data_smp'
 path_dc = ''#'data/DCs'
 
 path_dc = ''#'data/DCs'
@@ -23,17 +19,13 @@ bin_fac = None                 # no binning either 1 or None, 2x2 binning: bin_f
 norm_param = [3,5,20,40]
 crop_param = [10,15,80,60]
 oscillationParam = [30,1,1,1]
-numberPeriods = 2
+numberPeriods = 1
 
 im,ob = read_data(path_im,path_ob,path_dc)
 #im,ob=normalization(im,ob,*norm_param)
-#crop_param = [170,170,600,600]
 oscillation(im,ob,*oscillationParam,repeatedPeriod=True)
 #im,ob = cropped(im,ob,*crop_param)
 #im,ob=normalization(im,ob,*norm_param)
 #im, ob = binning(im,ob,bin_fac)
-#im, ob = win_filt_z(im,ob)
 ti, dpci, dfi, vis_map = createIm(im,ob,numberPeriods)
-#ti, dpci, dfi, vis_map = createIm_fft(im,ob)
-oscillation(im,ob,5,5,2,2)
 saveIm(ti, dpci, dfi, vis_map,name='period2',folder='folder',overWrite=True)
