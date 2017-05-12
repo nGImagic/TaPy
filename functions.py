4a96e8b3ceaa2a19b66054df5e747db5f61f7474
jacopovpsi <jacopo.valsecchi@psi.ch>
2017-05-12 09:06:28 +0200

    Decription

--- functions.py
+++ functions.py
@@ -110,27 +110,23 @@ def read_data(path_im,path_ob,path_dc):
     stack_ob = []
     for name in filenames_ob:
         full_path_name = path_ob+'/'+name
-#        print(full_path_name)
         if path_dc:
             stack_ob.append(readRead(full_path_name,im_a1)) #with dc
         else:
             stack_ob.append(readRead(full_path_name))   #without dc
     stack_ob = np.concatenate(stack_ob)
 
-    
 #    Projections
     filenames_im = [name for name in os.listdir(path_im) if name.lower().endswith(tuple(imExt))]
     filenames_im.sort()
     stack_im_ar = []
     for name in filenames_im:
         full_path_name = path_im+'/'+name
-#        print(full_path_name)
         if path_dc:
             stack_im_ar.append(readRead(full_path_name,im_a1)) #with dc
         else:
             stack_im_ar.append(readRead(full_path_name))   #without dc
     stack_im_ar = np.concatenate(stack_im_ar)
-    
     if np.shape(stack_im_ar) != np.shape(stack_ob):
             raise ValueError('Data and open beam have different shapes')
         
@@ -185,7 +181,6 @@ def roi(im,xROI,yROI,widthROI,heightROI,show=False,titleOne='Original image with
     if (0<=xROI<=im.shape[1] and 0<=xROI+widthROI<=im.shape[1] and 0<=yROI<=im.shape[0] and 0<=yROI+heightROI<=im.shape[0]):
         imROI = im[yROI:yROI+heightROI,xROI:xROI+widthROI]
         if show:
-#            vmin,vmax=im.min(),im.max()
             vmin,vmax=np.mean(im)-2*np.std(im),np.mean(im)+2*np.std(im)
             print(vmax, vmin)
             cmap='gray'
@@ -193,7 +188,6 @@ def roi(im,xROI,yROI,widthROI,heightROI,show=False,titleOne='Original image with
             gs = gridspec.GridSpec(1, 2,width_ratios=[4,1],height_ratios=[1,1]) 
             ax = plt.subplot(gs[0])
             ax2 = plt.subplot(gs[1])
-#            fig,(ax,ax2) = plt.subplots(1,2,sharex=False,sharey=False,figsize=(15,10))
             ax.imshow(im,vmin=vmin, vmax=vmax,interpolation='nearest',cmap=cmap)
             rectNorm = patches.Rectangle((xROI,yROI),widthROI,heightROI,linewidth=1,edgecolor='m',facecolor='none')
             ax.add_patch(rectNorm)
@@ -252,9 +246,7 @@ def cropped(stack_im,stack_ob,xROI=xROI,yROI=yROI,widthROI=widthROI,heightROI=he
     """
     stack_im_ar = [roi(im=stack_im[0],xROI=xROI,yROI=yROI,widthROI=widthROI,heightROI=heightROI,show=show,titleTwo='Cropped region',shape=True)]
     for i in stack_im[1:]:
-        stack_im_ar.append(roi(im=i,xROI=xROI,yROI=yROI,widthROI=widthROI,heightROI=heightROI,show=False))
-#    stack_im_ar = [roi(im=i,xROI=xROI,yROI=yROI,widthROI=widthROI,heightROI=heightROI,show=True) for i in stack_im]
-    
+        stack_im_ar.append(roi(im=i,xROI=xROI,yROI=yROI,widthROI=widthROI,heightROI=heightROI,show=False))    
     stack_ob_ar = [roi(im=i,xROI=xROI,yROI=yROI,widthROI=widthROI,heightROI=heightROI,show=False) for i in stack_ob]
     
     return(np.asarray(stack_im_ar),np.asarray(stack_ob_ar))
@@ -353,8 +345,6 @@ def oscillation(stack_im,stack_ob,xROI=xROI,yROI=yROI,widthROI=widthROI,heightRO
     stack_im_ar = [l[yROI:yROI+heightROI,xROI:xROI+widthROI].mean() for l in stack_im] 
 
     if repeatedPeriod:
-#        stack_ob_ar += stack_ob_ar[1:]  #without step
-#        stack_im_ar += stack_im_ar[1:]  #without step
         stack_ob_ar += stack_ob_ar
         stack_im_ar += stack_im_ar
         titleTwo += ' repetead period'
@@ -425,20 +415,18 @@ def matrix(stack_im,numberPeriods):
     
     stack_imReshaped = np.reshape(stack_im,[shapeStack[0],shapeStack[1]*shapeStack[2]])
     rangeStack = range(shapeStack[0])
-    
     for j in rangeStack:
         B[j][0] = 1.0
         B[j][1] = np.cos(2*np.pi*rangeStack[j]*numberPeriods/(shapeStack[0]-1))
         B[j][2] = np.sin(2*np.pi*rangeStack[j]*numberPeriods/(shapeStack[0]-1))
     B = np.matrix(B)
-    
     G = (B.T * B).I * B.T
-#    print(np.shape(G),np.shape(stack_imReshaped))
     A = (G*stack_imReshaped)
     offSet,absoluteAmpl,absPhase = A[0,:],A[1,:],A[2,:]
     a0 = np.reshape(np.asarray(offSet),[shapeStack[1],shapeStack[2]])
     a1 = np.reshape(np.sqrt(np.asarray(absoluteAmpl)**2+np.asarray(absPhase)**2),[shapeStack[1],shapeStack[2]])
     phi = np.reshape(np.arctan((np.asarray(absPhase)/np.asarray(absoluteAmpl))),[shapeStack[1],shapeStack[2]])
+    
     return a0,a1,phi
      
 def reductionMatrix(stack_im,stack_ob,numberPeriods):
@@ -467,61 +455,63 @@ def reductionMatrix(stack_im,stack_ob,numberPeriods):
 
 def createIm_fft(stack_im,stack_ob):
     """
-    createIm_fft(): it applies the fourier component analysis to retrieve dfi, ti, dpci and visibility map
+    Apply the fourier component analysis to retrieve dfi, ti, dpci and visibility map.
 
     Parameters
     ----------
+    stack_im : array_like
+        Input stack of projections.
+    
+    stack_ob : array_like
+        Input stack of open beam.
     
     Returns
     -------
-    
+    ti : array_like
+        Reconstructed fft trasmission array.
+        
+    dpci : array_like
+        Reconstructed fft DPCI array.
+        
+    dfi : array_like
+        Reconstructed fft DFI array.
+        
+    visi : array_like
+        Reconstructed fft visibility array.
     Notes
     -----
     """
     ## Projection
     shapeStack_im = np.shape(stack_im)
-    ###TODO: function for number of periods
-    
     stack_imReshaped = np.reshape(stack_im,[shapeStack_im[0],shapeStack_im[1]*shapeStack_im[2]])
-    
     steps = len(stack_imReshaped[:,:])
-    
     outfft_a_im = []
     outfft_b_im = []
     outfft_c_im = []
-    
     for i in range(len(stack_imReshaped[0,:])):
         fft_osc = np.fft.rfft(stack_imReshaped[:,i])/(steps-1)
         outfft_a_im.append(fft_osc[0])
         outfft_b_im.append(2*np.sqrt(np.real(fft_osc[1])**2+np.imag(fft_osc[1])**2))
         outfft_c_im.append(-np.angle(fft_osc[1]))
-    
     outfft_a_im = np.reshape(outfft_a_im, [shapeStack_im[1],shapeStack_im[2]])
     outfft_b_im = np.reshape(outfft_b_im, [shapeStack_im[1],shapeStack_im[2]])
     outfft_c_im = np.reshape(outfft_c_im, [shapeStack_im[1],shapeStack_im[2]])
 
     ## open beam
     shapeStack_ob = np.shape(stack_ob)
-    ###TODO: function for number of periods
-    
     stack_obReshaped = np.reshape(stack_ob,[shapeStack_ob[0],shapeStack_ob[1]*shapeStack_ob[2]])
-    
     steps = len(stack_obReshaped[:,:])
-
     outfft_a_ob = []
     outfft_b_ob = []
     outfft_c_ob = []
-    
     for i in range(len(stack_obReshaped[0,:])):
         fft_osc = np.fft.rfft(stack_obReshaped[:,i])/(steps-1)
         outfft_a_ob.append(fft_osc[0])
         outfft_b_ob.append(2*np.sqrt(np.real(fft_osc[1])**2+np.imag(fft_osc[1])**2))
         outfft_c_ob.append(-np.angle(fft_osc[1]))
-
     outfft_a_ob = np.reshape(outfft_a_ob, [shapeStack_ob[1],shapeStack_ob[2]])
     outfft_b_ob = np.reshape(outfft_b_ob, [shapeStack_ob[1],shapeStack_ob[2]])
     outfft_c_ob = np.reshape(outfft_c_ob, [shapeStack_ob[1],shapeStack_ob[2]])
-    
     ti = np.real(outfft_a_im)/np.real(outfft_a_ob)
     dpci = outfft_c_im - outfft_c_ob
     dfi = (np.real(outfft_b_im)*np.real(outfft_a_ob))/(np.real(outfft_b_ob)*np.real(outfft_a_im))
@@ -532,11 +522,32 @@ def createIm_fft(stack_im,stack_ob):
 
 def createIm(stack_im,stack_ob,numberPeriods):
     """
+    Process the projections and open beam stacks to retrieve dfi, ti, dpci and visibility map.
+    
     Parameters
     ----------
+    stack_im : array_like
+        Input stack of projections.
     
+    stack_ob : array_like
+        Input stack of open beam.
+        
+    numberPeriods : float_like
+        Numbers or fraction of stepped period.
+        
     Returns
     -------
+    TI : array_like
+        Reconstructed trasmission array.
+        
+    DPCI : array_like
+        Reconstructed DPCI array.
+        
+    DFI : array_like
+        Reconstructed DFI array.
+        
+    VIS_map : array_like
+        Reconstructed visibility array.
     
     Notes
     -----
@@ -544,21 +555,39 @@ def createIm(stack_im,stack_ob,numberPeriods):
     imParam,obParam = reductionMatrix(stack_im,stack_ob,numberPeriods)
     TI = np.divide(imParam[0],obParam[0])
     DPCI = imParam[2]-obParam[2]
-    DPCI = np.arctan(np.tan(DPCI)) # unwrap solver problem
-#    from skimage.restoration import unwrap_phase
-#    DPCI = unwrap_phase(DPCI)
-#    from unwrap import unwrap
-#    DPCI = unwrap(DPCI,wrap_around_axis_2=True)
-#    DPCI = np.unwrap(DPCI,0.1)
+    DPCI = np.arctan(np.tan(DPCI))
     DFI = np.divide(np.divide(imParam[1],imParam[0]),np.divide(obParam[1],obParam[0]))
     VIS_map = np.divide(obParam[1],obParam[0])
+    
     return TI, DPCI, DFI, VIS_map
     
 def saveIm(ti,dpci,dfi,vis_map,name='name',folder='folder',overWrite=False):
     """
+    Save the images to specific folder.
+    
     Parameters
     ----------
+    ti : array_like
+        Input reconstructed trasmission array.
+        
+    dpci : array_like
+        Input reconstructed DPCI array.
+        
+    dfi : array_like
+        Input reconstructed DFI array.
+        
+    vis_map : array_like
+        Input reconstructed visibility array.
     
+    name : sring_like
+        Specific name for the images.
+        
+    folder : string_like or False
+        Path of the folder where to save the oscillation plot, if False it doesn't save the plot.
+        
+    overWrite : boolean
+        If it's True it overwrite the images with the same name.
+        
     Returns
     -------
     
@@ -575,13 +604,26 @@ def saveIm(ti,dpci,dfi,vis_map,name='name',folder='folder',overWrite=False):
     
 def binning(stack_im,stack_ob,bin_fac=None):
     """
-    binning()
+    Bin the projections and open beam stacks. 
     
     Parameters
     ----------
+    stack_im : array_like
+        Input stack of projections.
+    
+    stack_ob : array_like
+        Input stack of open beam.
+        
+    bin_fac : int_like
+        Cluster of pixels into a single pixel.
     
     Returns
     -------
+    stack_im_bin : array_like
+            A stack containing all the binned projections.
+    
+    stack_ob_bin : array_like
+            A stack containing all the binned references.  
     
     Notes
     -----
@@ -598,7 +640,6 @@ def binning(stack_im,stack_ob,bin_fac=None):
     else:
         stack_im_bin = stack_im
         
-    
     num_ob,x_ob,y_ob = np.shape(stack_ob)
     stack_ob_tmp = list()
     if bin_fac:
@@ -612,82 +653,3 @@ def binning(stack_im,stack_ob,bin_fac=None):
         stack_ob_bin = stack_ob
         
     return stack_im_bin, stack_ob_bin
-
-
-def win_filt_z(stack_im,stack_ob):
-    """
-    med_filt_z(): Only use for very low DFI values and test before use to see if the results are better!!!
-   
-    Parameters
-    ----------
-    
-    Returns
-    -------
-    
-    Notes
-    -----
-    """
-    shapeStack_ob_org = np.shape(stack_ob)
-    stack_ob=np.append(stack_ob,np.delete(stack_ob,0,0), axis=0)
-    shapeStack_ob = np.shape(stack_ob)
-    
-    
-    stack_obReshaped = np.reshape(stack_ob,[shapeStack_ob[0],shapeStack_ob[1]*shapeStack_ob[2]])
-    stack_obReshaped = wiener(stack_obReshaped,[3,1])
-    stack_obReshaped = np.split(stack_obReshaped,[-(shapeStack_ob_org[0]-1)],0)[0]
-
-
-    ob = np.reshape(stack_obReshaped, [shapeStack_ob_org[0], shapeStack_ob_org[1],shapeStack_ob_org[2]])
-    
-    shapeStack_im_org = np.shape(stack_im)
-    stack_im=np.append(stack_im,np.delete(stack_im,0,0), axis=0)
-    shapeStack_im = np.shape(stack_im)
-    
-    
-    stack_imReshaped = np.reshape(stack_im,[shapeStack_im[0],shapeStack_im[1]*shapeStack_im[2]])
-    stack_imReshaped = wiener(stack_imReshaped,[3,1])
-    stack_imReshaped = np.split(stack_imReshaped,[-(shapeStack_im_org[0]-1)],0)[0]
-    im = np.reshape(stack_imReshaped, [shapeStack_im_org[0], shapeStack_im_org[1],shapeStack_im_org[2]])
-    
-    return im, ob
-
-def splitNewRoutine_BOA(path,firstSpinFlipperON=True):
-    """
-    when you acquire nGI data at BOA with the new routing (spinON spinOFF and step) this function splits the data into two folders
-   
-    Parameters
-    ----------
-    
-    Returns
-    -------
-    
-    Notes
-    -----
-    """
-    import re
-    
-    imExt = ['.fits','.tiff','.tif','.hdf','.h4','.hdf4','.he2','h5','.hdf5','.he5']
-    filenames = [name for name in os.listdir(path) if name.lower().endswith(tuple(imExt)) or re.search(r'\.bad\d+$', name)]
-    if all(re.search(r'\.bad\d+$', name) for name in filenames):
-        reshapedFilenamesBad =[]
-        print('All files ends .bad####')
-        for name in filenames:
-            nameSplit = re.split('_00001|\.bad',name)
-            nameSplit = nameSplit[0]+nameSplit[2].zfill(5)+nameSplit[1]
-            reshapedFilenamesBad.append(nameSplit)
-
-    for bad,badResh in zip(filenames,reshapedFilenamesBad):
-        print('old:', bad, '\nnew',badResh,2*'\n')
-        os.rename(path+'/'+bad,path+'/'+badResh)
-    filenames = reshapedFilenamesBad
-    
-    spin_ON,spin_OFF = '/spin_ON','/spin_OFF'
-    if not os.path.exists(path+spin_ON):
-        os.makedirs(path+spin_ON) 
-    if not os.path.exists(path+spin_OFF):
-        os.makedirs(path+spin_OFF)        
-    if firstSpinFlipperON:
-        for odd in filenames[::2]:
-            os.rename(path+'/'+odd,path+'/'+spin_ON+'/'+odd)
-        for even in filenames[1::2]:
-            os.rename(path+'/'+even,path+'/'+spin_OFF+'/'+even) 
