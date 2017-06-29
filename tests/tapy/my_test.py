c2e719509b18654f44510325eb998f5e24b43961
JeanBilheux <bilheuxjm@ornl.gov>
2017-06-29 16:28:12 -0400

    Prepare folder to run tests

---
+++ tests/tapy/my_test.py
@@ -0,0 +1,17 @@
+
+import unittest
+import numpy as np
+import os
+from PIL import Image
+
+#from tapy.calculate_radial_profile import CalculateRadialProfile
+
+class TestClass(unittest.TestCase):
+    
+    def setUp(self):    
+        pass
+        
+    def test_default_initialization(self):
+        """assert if all parameters are coorectly set up when no parameters passed in"""
+        pass       
+
