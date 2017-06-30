3bf4e63aa4d27702d27c0d39d83f527a15cd99f7
JeanBilheux <bilheuxjm@ornl.gov>
2017-06-30 11:48:01 -0400

    Code that load and test loading of fits image

--- tapy/grating_interferometer.py
+++ tapy/grating_interferometer.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+from astropy.io import fits
+import numpy as np
 
 
 class GratingInterferometer(object):
+    
+    data = []
     
     def __init__(self):
         pass
@@ -14,8 +18,6 @@ class GratingInterferometer(object):
         ----------
         path : string_like
             Path of the input file with his extention.
-        dc : array_like
-            An array containing the dark current data.
     
         Notes
         -----
@@ -25,6 +27,33 @@ class GratingInterferometer(object):
     
         my_file = Path(file_name)
         if my_file.is_file():
-            pass
+            if file_name.lower().endswith('.fits'):
+                try:
+                    self.load_fits(my_file)
+                except OSError:
+                    import fitsio
+                    im_a1.append(fitsio.read(path))
+            elif file_name.lower().endswith(('.tiff','.tif')) :
+                im_a1.append(np.asarray(Image.open(path)))
+            elif file_name.lower().endswith(('.hdf','.h4','.hdf4','.he2','h5','.hdf5','.he5')): 
+                # change here the hierarchy 
+                hdf = h5py.File(path,'r')['entry']['data']['data'].value    
+                for iScan in hdf:
+                    im_a1.append(iScan)
+            else:
+                raise OSError('file extension not yet implemented....Do it your own way!')     
+            #im_a1 = np.asarray(im_a1)-dc            
         else:
             raise OSError("The file name does not exist")
+        
+    def load_fits(self, file_name):
+        '''load fits image
+        
+        Parameters
+        ----------
+           full file name of fits image
+        '''
+        temp = fits.open(file_name,ignore_missing_end=True)[0].data
+        if len(temp.shape) == 3:
+            temp = temp.reshape(temp.shape[1:])                
+        self.data = temp
--- tests/tapy/my_test.py
+++ tests/tapy/my_test.py
@@ -10,7 +10,8 @@ from tapy.grating_interferometer import GratingInterferometer
 class TestClass(unittest.TestCase):
     
     def setUp(self):    
-        pass
+        _file_path = os.path.dirname(__file__)
+        self.data_path = os.path.abspath(os.path.join(_file_path, '../../data/'))
         
     def test_bad_file_name_raise_ioerror(self):
         """assert error is raised when wrong input data file name is given"""
@@ -23,3 +24,17 @@ class TestClass(unittest.TestCase):
         o_grating = GratingInterferometer()
         self.assertRaises(OSError, o_grating.load)
         
+    def test_loading_correct_fits(self):
+        '''assert fits file is correctd loaded'''
+        fits_file = self.data_path + '/fits/image001.fits'
+        o_grating = GratingInterferometer()
+        o_grating.load(file_name=fits_file)
+        data = o_grating.data
+        [height, width] = np.shape(data)
+        self.assertEqual(np.shape(data), (5, 5))
+        expected_data = np.ones([5,5])
+        for col in np.arange(5):
+            expected_data[:,col] = col        
+        self.assertTrue((data == expected_data).all())
+
+    
