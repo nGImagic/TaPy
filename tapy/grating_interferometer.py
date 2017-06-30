0687dbd9422c7a6b2709221951095cd72bb68d7e
JeanBilheux <bilheuxjm@ornl.gov>
2017-06-30 13:22:48 -0400

    Created fake tiff and fits images (light) for unit test. Worked on tiff reader

--- tapy/grating_interferometer.py
+++ tapy/grating_interferometer.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 from astropy.io import fits
 import numpy as np
+from PIL import Image
 
 
 class GratingInterferometer(object):
     
-    data = []
+    sample = []
+    ob = []
     
     def __init__(self):
         pass
     
-    def load(self, file_name=''):
+    def load(self, file_name='', data_type='sample'):
         """
         Function to read data from the specified path, it can read FITS, TIFF and HDF.
     
@@ -18,6 +20,7 @@ class GratingInterferometer(object):
         ----------
         path : string_like
             Path of the input file with his extention.
+        data_type: ['sample', 'df']
     
         Notes
         -----
@@ -27,14 +30,11 @@ class GratingInterferometer(object):
     
         my_file = Path(file_name)
         if my_file.is_file():
+            data = []
             if file_name.lower().endswith('.fits'):
-                try:
-                    self.load_fits(my_file)
-                except OSError:
-                    import fitsio
-                    im_a1.append(fitsio.read(path))
+                data = self.load_fits(my_file)
             elif file_name.lower().endswith(('.tiff','.tif')) :
-                im_a1.append(np.asarray(Image.open(path)))
+                data = self.load_tiff(my_file)
             elif file_name.lower().endswith(('.hdf','.h4','.hdf4','.he2','h5','.hdf5','.he5')): 
                 # change here the hierarchy 
                 hdf = h5py.File(path,'r')['entry']['data']['data'].value    
@@ -42,7 +42,12 @@ class GratingInterferometer(object):
                     im_a1.append(iScan)
             else:
                 raise OSError('file extension not yet implemented....Do it your own way!')     
-            #im_a1 = np.asarray(im_a1)-dc            
+            
+            if data_type == 'sample':
+                self.sample = data
+            else:
+                self.ob = data
+
         else:
             raise OSError("The file name does not exist")
         
@@ -53,7 +58,25 @@ class GratingInterferometer(object):
         ----------
            full file name of fits image
         '''
-        temp = fits.open(file_name,ignore_missing_end=True)[0].data
-        if len(temp.shape) == 3:
-            temp = temp.reshape(temp.shape[1:])                
-        self.data = temp
+        try:
+            temp = fits.open(file_name,ignore_missing_end=True)[0].data
+            if len(temp.shape) == 3:
+                temp = temp.reshape(temp.shape[1:])                
+            return temp
+        except OSError:
+            raise OSError("Unable to read the FITS file provided!")
+        
+    def load_tiff(self, file_name):
+        '''load tiff image
+        
+        Parameters:
+        -----------
+           full file name of tiff image
+        '''
+        try:
+            return np.asarray(Image.open(file_name))
+        except:
+            raise OSError("Unable to read the TIFF file provided!")
+            
+    
+#    def dark_field_correction
--- tests/tapy/my_test.py
+++ tests/tapy/my_test.py
@@ -11,7 +11,7 @@ class TestClass(unittest.TestCase):
     
     def setUp(self):    
         _file_path = os.path.dirname(__file__)
-        self.data_path = os.path.abspath(os.path.join(_file_path, '../../data/'))
+        self.data_path = os.path.abspath(os.path.join(_file_path, '../data/'))
         
     def test_bad_file_name_raise_ioerror(self):
         """assert error is raised when wrong input data file name is given"""
@@ -24,12 +24,12 @@ class TestClass(unittest.TestCase):
         o_grating = GratingInterferometer()
         self.assertRaises(OSError, o_grating.load)
         
-    def test_loading_correct_fits(self):
+    def test_loading_sample_fits(self):
         '''assert fits file is correctd loaded'''
-        fits_file = self.data_path + '/fits/image001.fits'
+        fits_file = self.data_path + '/image001.fits'
         o_grating = GratingInterferometer()
         o_grating.load(file_name=fits_file)
-        data = o_grating.data
+        data = o_grating.sample
         [height, width] = np.shape(data)
         self.assertEqual(np.shape(data), (5, 5))
         expected_data = np.ones([5,5])
@@ -37,4 +37,22 @@ class TestClass(unittest.TestCase):
             expected_data[:,col] = col        
         self.assertTrue((data == expected_data).all())
 
-    
+    def test_loading_format_not_supported(self):
+        '''assert error is raised when format is not supported'''
+        file = self.data_path + '/format_not_supported.txt'        
+        o_grating = GratingInterferometer()
+        self.assertRaises(OSError, o_grating.load, file_name=file)
+        
+    def test_loading_sample_tiff(self):
+        '''assert tiff file is corrected loaded'''
+        fits_file = self.data_path + '/image001.tif'
+        o_grating = GratingInterferometer()
+        o_grating.load(file_name=fits_file)
+        data = o_grating.sample
+        [height, width] = np.shape(data)
+        self.assertEqual(np.shape(data), (5, 5))
+        expected_data = np.ones([5,5])
+        for col in np.arange(5):
+            expected_data[:,col] = col        
+        self.assertTrue((data == expected_data).all())
+        
